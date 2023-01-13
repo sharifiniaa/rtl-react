51a558ece11ce0291d01ccf1c8d7a01a6d8d00d3
hossein.sha <hossein.sh@780.team>
2023-01-13 17:14:55 +0330

    test(counter): write user interactions test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
-import { Application } from './components/application/application';
-import { Skills } from './components/skills/skills';
+// import { Application } from './components/application/application';
+import { Counter } from './components/counter/counter';
+// import { Skills } from './components/skills/skills';
 
 function App() {
   return (
     <div className="App">
-      <Application/>
-      <Skills skills={['HTML', 'CSS']}/>
+      {/* <Application/>
+      <Skills skills={['HTML', 'CSS']}/> */}
+      <Counter/>
     </div>
   );
 }
