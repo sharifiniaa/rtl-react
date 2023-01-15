083623387e41fbaea2150a1891a124c469f28301
hossein.sha <hossein.sh@780.team>
2023-01-15 22:31:17 +0330

    test(providers): test and create utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import './App.css';
+
+import { AppProviders } from "./providers/app-providers";
+import { MuiMode } from "./components/mui/MuiMode";
 // import { Application } from './components/application/application';
-import { Counter } from './components/counter/counter';
+// import { Counter } from './components/counter/counter';
 // import { Skills } from './components/skills/skills';
 
 function App() {
   return (
+    <AppProviders>
     <div className="App">
+      <MuiMode/>
       {/* <Application/>
       <Skills skills={['HTML', 'CSS']}/> */}
-      <Counter/>
+      {/* <Counter/> */}
     </div>
+    </AppProviders>
   );
 }
 
